966878d3371b68532986eaaa9e0734581bfcbbc7
Exubient <henrynicholas09@gmail.com>
2017-01-23 22:48:15 -0600

    done with assignment1 IOS

--- IOS/KimHyunJoong-hw1/KimHyunJoong-hw1/File.swift
+++ IOS/KimHyunJoong-hw1/KimHyunJoong-hw1/File.swift
@@ -8,69 +8,33 @@
 
 
 
-//3. Get and set instance methods for each private property, except the speed property,
-//which should only have a get method.
-//4. A method named ‘increaseSpeed’, with one argument named ‘speedChange’ of
-//integer type. Make sure the resulting speed is not outside the range of 0 to 150.
-//5. A method named ‘decreaseSpeed’, with one argument named ‘speedChange’ of type
-//integer. Make sure the resulting speed is not outside the range of 0 to 150.
-//6. A method named ‘description’ that will return the following string:
-//Make: <make>, Model: <model>, NumDoors: <number-of-doors>, Speed: <speed>
-//2. In main.swift:
-//1. Define a ‘main’ global-scope function, with no arguments. In this function:
-//1. Create 3 Automobile objects with properties that produce the desired output (see
-//item 3 below), using the create method.
-//2. Define a loop that iterates 10 times, calling the increaseSpeed method on each
-//Automobile object, passing in a random value that is returned from the
-//randomValueBetween function (see below). Each call to increaseSpeed should use
-//an argument value from a unique call to randomValueBetween. When calling the
-//randomValueBetween function use minimum and maximum values of 0 and 16,
-//respectively.
-//3. After the loop has completed, call each object’s description method to output their
-//final state.
-//4. The last thing the main function should do is output a message for which automobile
-//won the race, in this format: “<automobile make> <automobile model> won!!”. Or, in
-//the unusual event there is a tie, output “There was a tie!”.
-//1. Example: Honda Accord won!!
-//2. At the global level, call the ‘main’ global-scope function.
-//3. The output should look like this, with speed values probably different when your program
-//runs:
-//Make: Maserati, Model: GranTurismo, NumDoors: 2, Speed: 67
-//Make: Honda, Model: Accord, NumDoors: 4, Speed: 128
-//Make: Tesla, Model: S 90, NumDoors: 2, Speed: 35
-//Honda Accord won!!
-//4. Build and run the app, and verify the output is correct.
-//
-//
-
-
 import Foundation
 
 class Automobile {
-    private var _name:String = ""
+    private var _make:String = ""
     private var _model:String = ""
     private var _numberOfDoors:Int = 0
     private var _speed:Int = 0
     
-    init (_name:String, _model:String, _numberOfDoors:Int, _speed:Int){
-        self._name = _name
+    init (_make:String, _model:String, _numberOfDoors:Int, _speed:Int){
+        self._make = _make
         self._model = _model
         self._numberOfDoors = _numberOfDoors
         self._speed = _speed
     }
     
-    class func create(_name:String, _model:String, _numberOfDoors:Int, _speed:Int) -> Automobile{
-        return Automobile(_name:_name, _model:_model, _numberOfDoors:_numberOfDoors, _speed:_speed)
+    class func create(_make:String, _model:String, _numberOfDoors:Int, _speed:Int) -> Automobile{
+        return Automobile(_make:_make, _model:_model, _numberOfDoors:_numberOfDoors, _speed:_speed)
     }
     
-    func set_name(_name:String){
-        self._name = _name
+    func set_make(_make:String){
+        self._make = _make
     }
     
-    func get_name() -> String{
-        return self._name
+    func get_make() -> String{
+        return self._make
     }
-
+    
     func set_model(_model:String){
         self._model = _model
     }
@@ -86,14 +50,51 @@ class Automobile {
     func get_numberOfDoors()->Int{
         return self._numberOfDoors
     }
-
+    
     func get_speed() -> Int{
         return self._speed
     }
-
+    
+    func increaseSpeed(_speedChange:Int) -> Int{
+        
+        let _current = self._speed + _speedChange
+        while (_current < 0) || (_current > 150){
+            print("you have a problem")
+            break
+        }
+        self._speed = _current
+        return _current
+    }
+    
+    func decreaseSpeed(_speedChange:Int) -> Int{
+        
+        let _current = self._speed - _speedChange
+        while (_current < 0) || (_current > 150){
+            print("you have a problem")
+            break
+        }
+        self._speed = _current
+        return _current
+    }
+    
+    func description() -> String{
+        let result1 = "Make: " + self._make
+        let result2 = ", Model: " + self._model
+        let result3 = ", NumDoors " + String(self._numberOfDoors)
+        let result4 = ", Speed: " + String(self._speed)
+        return result1 + result2 + result3 + result4
+    }
+    
     
     
 }
+
+
+
+
+
+
+
 
 
 
--- IOS/KimHyunJoong-hw1/KimHyunJoong-hw1/main.swift
+++ IOS/KimHyunJoong-hw1/KimHyunJoong-hw1/main.swift
@@ -6,10 +6,63 @@
 //  Copyright © 2017 Hyun Joong Kim. All rights reserved.
 //
 
+
+//3. After the loop has completed, call each object’s description method to output their
+//final state.
+//4. The last thing the main function should do is output a message for which automobile
+//won the race, in this format: “<automobile make> <automobile model> won!!”. Or, in
+//the unusual event there is a tie, output “There was a tie!”.
+//1. Example: Honda Accord won!!
+//2. At the global level, call the ‘main’ global-scope function.
+//3. The output should look like this, with speed values probably different when your program
+//runs:
+//Make: Maserati, Model: GranTurismo, NumDoors: 2, Speed: 67
+//Make: Honda, Model: Accord, NumDoors: 4, Speed: 128
+//Make: Tesla, Model: S 90, NumDoors: 2, Speed: 35
+//Honda Accord won!!
+//4. Build and run the app, and verify the output is correct.
+//
+//
+
+
+
 import Foundation
 
+
+func randomValueBetween(min:UInt32, max:UInt32) -> UInt32 {
+    var randomValue:UInt32 = min + arc4random_uniform(UInt32(max - min + 1))
+    return randomValue
+}
+
 func main() {
-    print("Hello, World!")
+    var results:String=""
+
+    var car1 = Automobile.create(_make: "HyunDai", _model: "Avante", _numberOfDoors: 4, _speed: 20)
+    var car2 = Automobile.create(_make: "Audi", _model: "A8", _numberOfDoors: 4, _speed: 30)
+    var car3 = Automobile.create(_make: "Kia", _model: "cheap_bike", _numberOfDoors: 2, _speed: 10)
+    
+    for count in 0 ..< 10{
+        car1.increaseSpeed(_speedChange: Int(randomValueBetween(min: 0, max: 16)))
+        car2.increaseSpeed(_speedChange: Int(randomValueBetween(min: 0, max: 16)))
+        car3.increaseSpeed(_speedChange: Int(randomValueBetween(min: 0, max: 16)))
+    }
+    
+    print(car1.description())
+    print(car2.description())
+    print(car3.description())
+    
+    
+    let largest = max(car1.get_speed(), car2.get_speed(), car3.get_speed())
+
+    if car1.get_speed() == largest{
+        print(car1.get_make() + " " + car1.get_model() + " won!!")
+    }
+    else if car2.get_speed() == largest{
+        print(car2.get_make() + " " + car2.get_model() + " won!!")
+    }
+    else{
+        print(car3.get_make() + " " + car3.get_model() + " won!!")
+    }
 }
 
 main()
