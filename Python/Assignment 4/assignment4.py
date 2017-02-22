f398cbcd3bc7208e6a9c50d8f1fe4fb3cc1e2d31
Exubient <henrynicholas09@gmail.com>
2017-02-22 17:13:22 -0600

    P4 implemented

--- Python/Assignment 4/assignment4.py
+++ Python/Assignment 4/assignment4.py
@@ -5,13 +5,16 @@ The idea of this assignment is to get you used to some of the dynamic
 and functional features of Python and how they can be used to perform
 useful metaprogramming tasks to make other development tasks easier.
 """
+#Hyun Joong Kim
+#hk23356
+
 
 import functools
 import logging
 import sys
-
 def logcalls(prefix):
-    """A function decorator that logs the arguments and return value
+    """
+    A function decorator that logs the arguments and return value
     of the function whenever it is called.
 
     The output should be to sys.stderr in the format:
@@ -28,30 +31,29 @@ def logcalls(prefix):
     def f(arg):
         return arg
 
-        (This is a more refined version of what I did in class)
+    (This is a more refined version of what I did in class)
     """
     def decorator(func):
         @functools.wraps(func)
         def inner(*args, **kwargs):
-            arg_all_str = ",".join([repr(arg) for arg in args] + [item[0] + "=" + repr(item[1])for item in kwargs.items()])
-            sys.stderr.write(prefix + ": " + func.__name__ + "(" + arg_all_str + ")")
-            sys.stderr.write("\n")
-            sys.stderr.write(prefix + ": " + func.__name__ + " -> " + repr(func(*args, **kwargs)))
-            return func(*args, **kwargs)
+            value=func(*args,  **kwargs)
+            joined_args = ", ".join([repr(args) for args in args] + [kwargs[0] + "=" + repr(kwargs[1]) for kwargs in kwargs.items()])
+            sys.stderr.write(prefix +": " + func.__name__ + "(" + joined_args+ ")\n" ) 
+            sys.stderr.write(prefix + ": " + func.__name__ + " -> "+  repr(value) + "\n" )   
+            return value
         return inner
     return decorator
-
-
+                
 def module_test(mod):
-
-    """Run all the test functions in the module mod (which is a module object).
+    """
+    Run all the test functions in the module mod (which is a module object).
 
     A test function is a function with a name that begins with "test".
     All the test functions take no arguments and throw an exception if
     the test fails.
 
     For each test, print a block to stderr in the following format:
-      {test function name}: {either PASS or FAIL: {exception type name} {exception as a string}}
+      {test function name}: {either PASS or FAIL: {exception type name} {exception    as a string}}
       {test function doc string}
     For example of a test is defined:
       def testA():
@@ -64,7 +66,14 @@ def module_test(mod):
     Make sure you handle functions without doc strings without crashing
     (you can treat it as an empty doc string).
     """
-    func_list = [item[1] for item in mod.__dict__.items() if isinstance(item[0],str) and item[0].startswith('test')]    
-    sys.stderr.write("\n".join([func.__name__ + ": " + ("PASS" if func() else "FAIL") for func in func_list] + [ func.__doc__ for func in func_list if func.__doc__ is not None]))
-    sys.stderr.write("\n")
-    
+    for tests in mod.__dict__:
+        if tests.startswith('test'):
+            if callable(mod.__dict__[tests]):
+                if(mod.__dict__[tests])(): 
+                    sys.stderr.write (tests +': PASS')
+                else:
+                    sys.stderr.write (tests +': FAIL :' + mod.__dict__[tests])
+                sys.stderr.write('\n')
+                sys.stderr.write(mod.__dict__[tests].__doc__)
+                
+              
